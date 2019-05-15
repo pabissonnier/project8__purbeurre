2f90a9bab5b90fb97c4b1d82ab733c11c1296181
pabissonnier <pabissonnier@gmail.com>
2019-05-15 16:09:33 +0200

    .gitignore is now working

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\pabis\OneDrive\Desktop\OC\Projets\purbeurre\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
