e9c9ff529128fbdbc8cf5c490a7c2aa47540ed18
pabissonnier <pabissonnier@gmail.com>
2019-05-15 16:17:35 +0200

    Adding views for answer

--- answer/views.py
+++ answer/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+from .models import Products, User, UserList
+
+
+def index(request):
+    return render(request, 'answer/index.html')
+
+
+def search(request):
+    query = request.GET.get('query')
+    if not query:
+        products = Products.objects.all()
+    else:
+        products = Products.objects.filter(name__icontains=query)
+        if not products.exists():
+            message = "Aucun produit trouvé"
+    title = "Résultats pour la recherche : '%s'" % query
+    context = {
+        'products': products,
+        'title': title
+    }
+    return HttpResponse(render(request, 'answer/search.html', context))
+
+
+def app(request):
+    query = request.GET.get('app-query')
+    if not query:
+        products = Products.objects.all()
+    else:
+        products = Products.objects.filter(name__icontains=query)
+        if not products.exists():
+            message = "Aucun produit trouvé"
+    title = "Voici de meilleurs produits pour : '%s'" % query
+    context = {
+        'products': products,
+        'title': title,
+    }
+    return HttpResponse(render(request, 'answer/results.html', context))
--- purbeurre/urls.py
+++ purbeurre/urls.py
@@ -1,28 +1,21 @@
-"""purbeurre URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
 from django.conf import settings
+from django.conf.urls import include, url
+from django.contrib import admin
+# from user import views as user_views
+from answer import views as answer_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^$', answer_views.index, name='home'),
+    url(r'^answer/', answer_views.app, name='application'),
+    # url(r'^products/', include('products.urls')),
+    url(r'^search/$', answer_views.search, name='search-products'),
+    url(r'^admin/', admin.site.urls),
+    # url(r'^register/', user_views.register, name='register'),
 ]
+
 
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns = [
-        path('__debug__/', include(debug_toolbar.urls)),
+        url('__debug__/', include(debug_toolbar.urls)),
     ] + urlpatterns
