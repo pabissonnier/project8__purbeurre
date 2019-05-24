9f442c6806d438be0d767052bb8d3d7530e0f9cc
pabissonnier <pabissonnier@gmail.com>
2019-05-24 15:23:49 +0200

    Creation of sign in page'

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -128,3 +128,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 INTERNAL_IPS = ['127.0.0.1']
+
--- purbeurre/urls.py
+++ purbeurre/urls.py
@@ -3,6 +3,7 @@
 from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from users import views as user_views
 from answer import views as answer_views
 
@@ -13,6 +14,9 @@ urlpatterns = [
     url(r'^search/$', answer_views.search, name='search-products'),
     url(r'^admin/', admin.site.urls),
     url(r'^register/', user_views.register, name='register'),
+    url(r'^login/', auth_views.LoginView.as_view(template_name = 'users/login.html'), name='login'),
+    url(r'^logout/', auth_views.LogoutView.as_view(template_name = 'users/logout.html'), name='logout'),
+
 ]
 
 
