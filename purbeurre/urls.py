4fb7b1cfc1b28e3efb7cbcd5a5bd12d7ae968b7b
pabissonnier <pabissonnier@gmail.com>
2019-05-24 11:33:20 +0200

    Creation of the user form

--- purbeurre/urls.py
+++ purbeurre/urls.py
@@ -3,7 +3,7 @@
 from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
-# from user import views as user_views
+from users import views as user_views
 from answer import views as answer_views
 
 urlpatterns = [
@@ -12,7 +12,7 @@ urlpatterns = [
     url(r'^(?P<product_id>[0-9]+)/$', answer_views.detail, name='detail'),
     url(r'^search/$', answer_views.search, name='search-products'),
     url(r'^admin/', admin.site.urls),
-    # url(r'^register/', user_views.register, name='register'),
+    url(r'^register/', user_views.register, name='register'),
 ]
 
 
