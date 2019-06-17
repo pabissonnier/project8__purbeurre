d30360f150684bb35d9455aa360f003469f07d04
pabissonnier <pabissonnier@gmail.com>
2019-06-17 08:24:56 +0200

    Working on tests, adding a test folder and testing urls

---
+++ answer/tests/test_urls.py
@@ -0,0 +1,28 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from answer.views import index, app, app_sim, detail, search
+
+
+class TestUrls(SimpleTestCase):
+
+    def test_index_url_resolves(self):
+        url = reverse('home')
+        self.assertEquals(resolve(url).func, index)
+
+    def test_app_url_resolves(self):
+        url = reverse('application')
+        self.assertEquals(resolve(url).func, app)
+
+    def test_app_sim_url_resolves(self):
+        url = reverse('application_sim')
+        self.assertEquals(resolve(url).func, app_sim)
+
+    def test_search_url_resolves(self):
+        url = reverse('search-products')
+        self.assertEquals(resolve(url).func, search)
+
+    def test_detail_url_resolves(self):
+        url = reverse('detail', args=['product_id'])
+        self.assertEquals(resolve(url).func, detail) # doesn't work
+
+
