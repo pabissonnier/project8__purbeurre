22d8151667ee49f0bf8b8af4c86b9ef81d7122a2
pabissonnier <pabissonnier@gmail.com>
2019-07-03 09:51:38 +0200

    Launching to production

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -45,6 +46,9 @@ if os.environ.get('ENV') == 'PRODUCTION':
     )
 
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+    db_from_env = dj_database_url.config(conn_max_age=500)
+    DATABASES['default'].update(db_from_env)
 
 # Application definition
 
