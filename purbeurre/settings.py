0b148ffd68e6e531e6f17e52d36ac45aa3437b61
pabissonnier <pabissonnier@gmail.com>
2019-07-04 14:26:08 +0200

    Settings updated

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -149,7 +149,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = 'answer/static/'
+STATIC_URL = '/static/'
 
 INTERNAL_IPS = ['127.0.0.1']
 
