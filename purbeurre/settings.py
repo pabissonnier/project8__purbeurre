46d3340833b3a8d39c4ea98bec49303bc7d223ce
pabissonnier <pabissonnier@gmail.com>
2019-07-04 16:44:23 +0200

    Settings upgraded

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -38,7 +38,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'purbeurre/static'),
 )
 
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Application definition
