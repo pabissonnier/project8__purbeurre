524dd3be43a71d611f4cbf85747e18a5a7960b7a
pabissonnier <pabissonnier@gmail.com>
2019-07-04 14:34:47 +0200

    Settings upgraded

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -36,7 +36,7 @@ ALLOWED_HOSTS = ['purbeurre-yukalike.herokuapp.com']
 if os.environ.get('ENV') == 'PRODUCTION':
 
     # Static files settings
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
     # Extra places for collectstatic to find static files.
     STATICFILES_DIRS = (
