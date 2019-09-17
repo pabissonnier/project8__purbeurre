6646c56d08739c01093a63b46e6e112578a5d64a
pabissonnier <pabissonnier@gmail.com>
2019-09-17 15:03:53 +0200

    Change few stuff on settings and command for testing on travis

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -88,8 +87,8 @@ WSGI_APPLICATION = 'purbeurre.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'purbeurreprod',
-        'USER': 'pabisso',
+        'NAME': 'purbeurre1',
+        'USER': 'postgres',
         'PASSWORD': '458127',
         'HOST': 'localhost',
         'PORT': '5432',
