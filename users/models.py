5e6b878d2e114782a20ce74e9523cd3d64855930
pabissonnier <pabissonnier@gmail.com>
2019-11-17 20:33:36 +0100

    fuck

--- users/models.py
+++ users/models.py
@@ -6,5 +6,6 @@ from django.utils import timezone
 class Subscribe(models.Model):
     email_id = models.EmailField(null = True, blank = True)
     timestamp = models.DateTimeField(default=timezone.now)
+
     def __str__(self):
         return self.email_id
