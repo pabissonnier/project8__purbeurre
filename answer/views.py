04b5d8e7e638cf9ce3f48495e3aa62c9d3d57043
pabissonnier <pabissonnier@gmail.com>
2019-11-26 09:57:57 +0100

    Bio logo now available on search resultsé

--- answer/views.py
+++ answer/views.py
@@ -8,17 +8,6 @@ from dal import autocomplete
 from .models import Product
 
 
-class ProductAutocomplete(autocomplete.Select2QuerySetView):
-    def get_queryset(self):
-
-        qs = Product.objects.all()
-
-        if self.q:
-            qs = qs.filter(name__istartswith=self.q)
-
-        return qs
-
-
 def index(request):
     return render(request, 'answer/index.html')
 
