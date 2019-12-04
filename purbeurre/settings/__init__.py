f22cdd7bf5318c101bbddd6411e40aec8b764a01
pabissonnier <pabissonnier@gmail.com>
2019-12-04 16:45:14 +0100

    Moving media

--- purbeurre/settings/__init__.py
+++ purbeurre/settings/__init__.py
@@ -39,7 +39,7 @@ ALLOWED_HOSTS = ['127.0.0.1', 'purbeurre-yukalike.herokuapp.com', '178.62.25.139
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "../../media")
 MEDIA_URL = '/media/'
 
 STATICFILE_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
